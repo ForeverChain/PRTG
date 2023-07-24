a356ef59a5a1bb26028f9ccd8b684fb9fff9f6a3
monherdene <interactivemonh@gmail.com>
2023-07-24 14:32:15 +0800

    changes

--- WinFormsApp2/Form1.Designer.vb
+++ WinFormsApp2/Form1.Designer.vb
@@ -24,12 +24,18 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Label1 = New Label()
         Label2 = New Label()
+        DataGridView1 = New DataGridView()
+        deviceName = New DataGridViewTextBoxColumn()
+        IP = New DataGridViewTextBoxColumn()
+        Status = New DataGridViewTextBoxColumn()
+        message = New DataGridViewTextBoxColumn()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(356, 168)
+        Label1.Location = New Point(356, 31)
         Label1.Name = "Label1"
         Label1.Size = New Size(41, 15)
         Label1.TabIndex = 0
@@ -38,25 +44,62 @@ Partial Class Form1
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(356, 221)
+        Label2.Location = New Point(356, 68)
         Label2.Name = "Label2"
         Label2.Size = New Size(41, 15)
         Label2.TabIndex = 1
         Label2.Text = "Label2"
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {deviceName, IP, Status, message})
+        DataGridView1.Location = New Point(202, 107)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowTemplate.Height = 25
+        DataGridView1.Size = New Size(436, 331)
+        DataGridView1.TabIndex = 2
+        ' 
+        ' deviceName
+        ' 
+        deviceName.HeaderText = "Status"
+        deviceName.Name = "deviceName"
+        ' 
+        ' IP
+        ' 
+        IP.HeaderText = "Device name"
+        IP.Name = "IP"
+        ' 
+        ' Status
+        ' 
+        Status.HeaderText = "Ip address"
+        Status.Name = "Status"
+        ' 
+        ' message
+        ' 
+        message.HeaderText = "message"
+        message.Name = "message"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7.0F, 15.0F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(DataGridView1)
         Controls.Add(Label2)
         Controls.Add(Label1)
         Name = "Form1"
         Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents deviceName As DataGridViewTextBoxColumn
+    Friend WithEvents IP As DataGridViewTextBoxColumn
+    Friend WithEvents Status As DataGridViewTextBoxColumn
+    Friend WithEvents message As DataGridViewTextBoxColumn
 End Class
--- WinFormsApp2/Form1.vb
+++ WinFormsApp2/Form1.vb
@@ -2,11 +2,14 @@
 Imports PrtgAPI.Parameters
 Imports PrtgAPI.Request
 Imports System.Threading
+Imports System.Drawing
 Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 
 Public Class Form1
     Private prtgClient As PrtgClient
     Public cancellationToken As CancellationToken = CancellationToken.None
+
+    Private WithEvents dgvDevices As DataGridView
 
     Public Sub New()
         InitializeComponent()
@@ -38,14 +41,34 @@ Public Class Form1
             For Each device In devices
                 Dim sensors = Await prtgClient.GetSensorsAsync([Property].ParentId, device.Id)
                 For Each sensor In sensors
-                    If sensor.Status = PrtgAPI.Status.Down Then
-                        downCount += 1
-                    ElseIf sensor.Status = PrtgAPI.Status.Warning Then
-                        warningCount += 1
-                    ElseIf sensor.Status = PrtgAPI.Status.Up Then
-                        upCount += 1
-                    End If
+                    Dim statusText As String = GetStatusText(sensor.Status)
+
+                    Dim statusColor As Color
+                    Select Case sensor.Status
+                        Case PrtgAPI.Status.Down
+                            statusColor = Color.Red
+                        Case PrtgAPI.Status.Warning
+                            statusColor = Color.Yellow
+                        Case PrtgAPI.Status.Up
+                            statusColor = Color.Green
+                        Case Else
+                            statusColor = Color.Gray
+                    End Select
+
+                    Dim row As DataGridViewRow = New DataGridViewRow()
+                    row.CreateCells(DataGridView1, statusText, device.Name, device.Host, sensor.Message)
+                    row.Cells(0).Style.BackColor = statusColor
+                    DataGridView1.Rows.Add(row)
                 Next
+
+                ' Count the statuses
+                If sensors.Any(Function(s) s.Status = PrtgAPI.Status.Down) Then
+                    downCount += 1
+                ElseIf sensors.Any(Function(s) s.Status = PrtgAPI.Status.Warning) Then
+                    warningCount += 1
+                ElseIf sensors.All(Function(s) s.Status = PrtgAPI.Status.Up) Then
+                    upCount += 1
+                End If
             Next
 
             Label2.Text = String.Format("Sensors: {0} Up, {1} Warning, {2} Down", upCount, warningCount, downCount)
@@ -53,7 +76,37 @@ Public Class Form1
             ' Code to execute if there is an exception
             Console.WriteLine("Error connecting to PRTG server: " & ex.Message)
         End Try
+
+        AddHandler DataGridView1.CellPainting, AddressOf DataGridView1_CellPainting
     End Sub
+    Private Function GetStatusText(status As PrtgAPI.Status) As String
+        Select Case status
+            Case PrtgAPI.Status.Down
+                Return "Down"
+            Case PrtgAPI.Status.Warning
+                Return "Warning"
+            Case PrtgAPI.Status.Up
+                Return "Up"
+            Case Else
+                Return "Unknown"
+        End Select
+    End Function
+
+    Private Sub DataGridView1_CellPainting(ByVal sender As Object, ByVal e As DataGridViewCellPaintingEventArgs)
+        If e.ColumnIndex = 0 AndAlso e.RowIndex >= 0 Then ' Assuming the leftmost column index is 0
+            Dim statusCell As DataGridViewCell = DataGridView1.Rows(e.RowIndex).Cells(0)
+            If statusCell.Value IsNot Nothing AndAlso Not String.IsNullOrEmpty(statusCell.Value.ToString()) Then
+                Dim statusText As String = statusCell.Value.ToString().ToLower()
+                If statusText = "warning" Then
+                    e.CellStyle.ForeColor = Color.Black
+                Else
+                    e.CellStyle.Font = New Font(DataGridView1.Font, FontStyle.Bold)
+                    e.CellStyle.ForeColor = Color.White
+                End If
+            End If
+        End If
+    End Sub
+
 
     Private Sub label1_Click(ByVal sender As Object, ByVal e As EventArgs)
     End Sub
@@ -64,4 +117,10 @@ Public Class Form1
     Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
+
+    Private Sub Form1_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
 End Class
